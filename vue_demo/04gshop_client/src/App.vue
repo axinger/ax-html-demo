1ac74d795f037bb68875255bb3e163816b3d68b3
axinger <axinger@outlook.com>
2021-08-31 21:18:28 +0800

    update…

---
+++ vue_demo/04gshop_client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+    <router-view/>
+
+
+    <FooterGuide/>
+  </div>
+</template>
+
+
+<script>
+
+import FooterGuide from "./components/FooterGuide/FooterGuide";
+export default {
+
+  components:{
+    FooterGuide,
+  }
+}
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+// 整个应用组件的样式
+#app
+  width 100%
+  height 100%
+  background white
+</style>
---
+++ vue_demo/04gshop_client/src/views/Order/Order.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>oder</div>
+</template>
+
+<script>
+export default {
+  name: "Order"
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
